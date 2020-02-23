e2f9d647f99af13c9122f6bb1557fffbf32f01d4
Jan Sarenik <jajomojo@gmail.com>
2020-02-23 15:56:34 +0100

    Add mouse.sh run from ~/.xinitrc

---
+++ bin/mouse.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+xinput set-prop "bcm5974" "libinput Tapping Enabled" 1
+xinput set-prop "bcm5974" "libinput Natural Scrolling Enabled" 1
+xinput set-prop "bcm5974" "libinput Middle Emulation Enabled" 1
+
+xinput set-prop 'Microsoft Microsoft 5-Button Mouse with IntelliEye(TM)' "libinput Natural Scrolling Enabled" 1
