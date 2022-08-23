d0815c50f85112987b675b035a00f7260b8c0908
Jan Sarenik <jajomojo@gmail.com>
2022-08-23 11:38:38 +0200

    keyboard.sh: add vdtnsaoh.sh symlink
    When the layout is reset to US QWERTY, this helps to load the
    wanted keyboard quickly.
    
    Other minor changes to sync the file with live-used one.

--- bin/keyboard.sh
+++ bin/keyboard.sh
@@ -1,32 +1,42 @@
 #!/bin/sh
+#
+# keyboard.sh -> vdtnsaoh.sh (us keymap with dvorak-like key-presses)
+
+a="/$0"; a="${a%/*}"; a="${a:-.}"; a="${a##/}/"; BINDIR="$(cd $a; pwd)"
+ln -nsf keyboard.sh $BINDIR/vdtnsaoh.sh
 
 setxkbmap ${1:-'dvorak'}
 
 setxkbmap -option ""
+
+CAPSTATE=`xset -q | grep -Eo "[0-9]+: Caps Lock:\s+\S+" | awk '{print $4}'`
+test "$CAPSTATE" = "on" && xdotool key Caps_Lock
+
 xmodmap - <<EOF
 ! for help, see $ xmodmap -grammar
-
 !in /etc/X11/xorg.conf.d/20-keyboard.conf
 ! or by running setxkbmap -option "ctrl:nocaps"
 remove Lock = Caps_Lock
 keysym Caps_Lock = Control_L
 add Control = Control_L
-
 ! MacBook keys - Alt instead of Command, Right Command = Compose
 clear Mod4
 clear Mod1
 keycode 133 = Super_R
-!keycode 134 = Super_L
+keycode 134 = Super_L
 keysym Super_R = Multi_key
+keysym Alt_R = Multi_key
+! add Mod1 = Alt_L Super_L
 add Mod1 = Alt_L Super_L
-
+!add Mod4 = Alt_L Super_L
 ! Natural two-finger scrolling
-pointer = 1 2 3 5 4 7 6 8 9 10 11 12 13
+!pointer = 1 2 3 5 4 7 6 8 9 10 11 12 13
 !pointer = 1 2 3 5 4 7 6 8 9 10 11 12
 EOF
 
 #! Normal right Alt Compose
-xmodmap -e 'keysym Alt_R = Multi_key'
+#xmodmap -e 'keysym Alt_R = Multi_key'
+#setxkbmap -option "compose:ralt"
 
 
 #setxkbmap -option "ctrl:nocaps"
@@ -44,19 +54,3 @@ setxkbmap -query
 #     00: Caps Lock:   off    01: Num Lock:    off    02: Scroll Lock: off
 #     03: Compose:     off    04: Kana:        off    05: Sleep:       off
 # ...
-
-CAPSTATE=`xset -q | grep -Eo "[0-9]+: Caps Lock:\s+\S+" | awk '{print $4}'`
-test "$CAPSTATE" = "on" && xdotool key Caps_Lock
-
-##setxkbmap dvorak,il
-#setxkbmap dvorak
-
-##	setxkbmap -option ""
-##	setxkbmap -option "grp_led:scroll"
-##	setxkbmap -option "compose:ralt"
-##	setxkbmap -option "ctrl:nocaps"
-##
-##	setxkbmap -option "grp:rwin_toggle"
-#setxkbmap -option "compose:rctrl"
-
-## setxkbmap -query
