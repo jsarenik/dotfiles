4238b053f27462d9d7b6dc6efe05b56fb99821b3
Jan Sarenik <jajomojo@gmail.com>
2022-11-30 17:22:57 +0100

    Add mywatch.sh
    Similar to watch(1) but does not blink the screen.
    Uses some console_codes(4) magic.

---
+++ bin/mywatch.sh
@@ -0,0 +1,43 @@
+#!/busybox/ash
+
+TMP=/tmp/mywatch-$$
+cols=$(tput cols)
+sleep="2.0s"
+test "$1" = "-n" && { sleep=${2:-"2.0s"}; shift 2; }
+
+trap "cat $TMP 2>/dev/null; rm -f ${TMP}*; tput cnorm; exit" INT QUIT EXIT
+#reset; clear
+printf '\033[2J'
+
+printf "[?25l"
+printf "[H"
+
+O=""
+while true;
+do
+  datetime=$(date +"%Y-%m-%d %H:%M:%S")
+  datetimew=$(printf "%s" "$datetime" | wc -c)
+  move=$((cols-datetimew))
+  line="Every ${sleep}: $@ [${move}G${datetime}"
+  printf "%s\n" "${line}" > $TMP
+  echo >>$TMP
+  echo "$@" | $SHELL -s >>$TMP 2>&1
+  N=$(md5sum $TMP | cut -b-32)
+  if
+    test "$O" = "$N"
+  then
+    :
+  else
+    size_rows=$(cat $TMP | wc -l)
+    for i in $(seq $size_rows);
+    do printf "[K\n"
+    done > $TMP-emptyline
+    {
+    cat $TMP | paste - $TMP-emptyline | tr -d '\t'
+    #printf "[${size_rows}F"
+    printf "[H"
+    } | cat
+  fi
+  O=$N
+  sleep ${sleep}
+done
